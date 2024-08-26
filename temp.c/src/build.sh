157f05b81871f5f902014378d268241c2d2133dc
errornesss <errorisonlytemporary@proton.me>
2024-08-26 10:57:19 +1000

    temp.c changes

--- temp.c/src/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-out="../build/main"
-
-mkdir -p ../build
-gcc ./*.c -o $out
-$out
-# rm $out
--- temp.c/src/main.c
+++ temp.c/src/main.c
@@ -3,5 +3,6 @@
 #include "utils/utils.h"
 
 i32 main(/* i32 argc, char *argv[] */) {
+  printf("hello world\n");
   return 0;
 }
