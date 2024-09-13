80d1fc9943651f9eec9ffae11c3b99237d1bff4b
errornesss <errorisonlytemporary@proton.me>
2024-09-13 10:59:03 +1000

    changed build script

--- opengl.temp/build.sh
+++ opengl.temp/build.sh
@@ -7,11 +7,12 @@ popd() {
   command popd "$@" > /dev/null
 }
 
+cc="zig cc"
 out="main"
 
 mkdir -p bin/
 pushd src/
-gcc -o ../bin/$out *.c utils/*.c include/glad.c -Iinclude/ -lglfw -ldl -lGL
+$cc -o ../bin/$out *.c utils/*.c include/glad.c -Iinclude/ -lglfw -ldl -lGL
 pushd ../bin
 ./$out
-# rm $out
+rm $out
