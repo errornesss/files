a91c3e5b8980a1b4a6d2e2d9a956ff3df98ea2b6
errornesss <errorisonlytemporary@proton.me>
2024-10-22 22:26:54 +1100

    change build script

--- opengl.temp/build.sh
+++ opengl.temp/build.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
 
-pushd() {
-  command pushd "$@" > /dev/null
-}
-popd() {
-  command popd "$@" > /dev/null
-}
-
 cc="zig cc"
 out="main"
 incsrc="include/glad/glad.c include/stb/stb_image.c"
 
 mkdir -p bin/
-pushd src/
+cd src/
 $cc -o ../bin/$out main.c utils/*.c $incsrc -Iinclude/ -lGL -lglfw
-pushd ../bin
+cd ../bin
 ./$out
 rm $out
