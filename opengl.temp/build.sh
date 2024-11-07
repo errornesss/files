83a6f1013dde124591b8bd486a148972b2cf5961
errornesss <errorisonlytemporary@proton.me>
2024-11-07 15:28:39 +1100

    change build script

--- opengl.temp/build.sh
+++ opengl.temp/build.sh
@@ -4,7 +4,7 @@ cc="zig cc"
 out="main"
 incsrc="include/glad/glad.c include/stb/stb_image.c"
 
-# mkdir -p bin/
+mkdir -p bin/
 # cd src/
 $cc src/main.c -o bin/$out src/utils/*.c $incsrc -lGL -lglfw
 cd bin
