f31b44f3bd2f366215852b8d9ad36b94ff54dbd7
errornesss <errorisonlytemporary@proton.me>
2024-11-07 16:22:36 +1100

    added glfw.a

--- opengl.temp/build.sh
+++ opengl.temp/build.sh
@@ -2,11 +2,11 @@
 
 cc="zig cc"
 out="main"
-incsrc="include/glad/glad.c include/stb/stb_image.c"
+incsrc="include/glad/glad.c include/glfw/glfw.a include/stb/stb_image.c"
 
 mkdir -p bin/
 # cd src/
-$cc src/main.c -o bin/$out src/utils/*.c $incsrc -lGL -lglfw
+$cc src/main.c -o bin/$out src/utils/*.c $incsrc
 cd bin
 ./$out
 rm $out
